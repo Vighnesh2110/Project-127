6aa3029cd6ce18702d0a90356087994a9e41d1ac
Vighnesh2110 <74961877+Vighnesh2110@users.noreply.github.com>
2023-04-04 16:45:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+song1 = " ";
+song2 = " ";
+
+song1_status = " ";
+song2_status = " ";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload(){
+    song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas = createCanvaas(600, 500);
+    canvas.center();
+
+    video = createCapture(Video);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}  
